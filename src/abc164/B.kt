2e4c39007e44e20060f1d324fcf7fa9b461f078e
Kei Negishi <56011598+keinegishi@users.noreply.github.com>
2020-06-08 02:35:20 +0900

    commit

---
+++ src/abc164/B.kt
@@ -0,0 +1,73 @@
+package abc164
+
+fun main() {
+    var (a, b, c, d) = readListInt()
+    for (i in 1..10000) {
+        if (i % 2 != 0) {
+            c -= b
+            if (c <= 0) {
+                println("Yes")
+                return
+            }
+        } else  {
+            a -= d
+            if (a <= 0) {
+                println("No")
+                return
+            }
+        }
+    }
+}
+
+private fun read() = readLine()!!
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readListString() = read().split(" ")
+
+private fun readListInt() = readListString().map { it.toInt() }
+
+private fun readListLong() = readListString().map { it.toLong() }
+
+private fun readListDouble() = readListString().map { it.toDouble() }
+
+private fun readMatrixString(n: Int): List<List<String>> {
+    val matrix = mutableListOf<List<String>>()
+    for (i in 0 until n) {
+        val list = readListString()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readMatrixInt(n: Int): List<List<Int>> {
+    val matrix = mutableListOf<List<Int>>()
+    for (i in 0 until n) {
+        val list = readListInt()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readMatrixDouble(n: Int): List<List<Double>> {
+    val matrix = mutableListOf<List<Double>>()
+    for (i in 0 until n) {
+        val list = readListDouble()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readMatrixLong(n: Int): List<List<Long>> {
+    val matrix = mutableListOf<List<Long>>()
+    for (i in 0 until n) {
+        val list = readListLong()
+        matrix.add(list)
+    }
+    return matrix
+}
+
