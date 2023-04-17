2d6d4f71e2f178ba9b3b4b8ba52aed00a18f8694
k-negishi <56011598+k-negishi@users.noreply.github.com>
2023-04-17 22:38:17 +0900

    2023-04-17

---
+++ src/abc201_300/abc297/B.kt
@@ -0,0 +1,75 @@
+package abc201_300.abc297
+
+fun main() {
+    val s = read()
+
+    val firstB = s.indexOf("B")
+    val lastB = s.lastIndexOf("B")
+
+    val firstR = s.indexOf("R")
+    val lastR = s.lastIndexOf("R")
+    val kIndex = s.indexOf("K")
+
+    if ((firstB + lastB) % 2 != 0
+        && (kIndex in (firstR + 1) until lastR)) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+
+
+}
+
+
+private fun read() = readLine()!!
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readStringList(): MutableList<String> = read().split(" ").toMutableList()
+
+private fun readIntList(): MutableList<Int> = readStringList().map { it.toInt() }.toMutableList()
+
+private fun readLongList(): MutableList<Long> = readStringList().map { it.toLong() }.toMutableList()
+
+private fun readDoubleList(): MutableList<Double> = readStringList().map { it.toDouble() }.toMutableList()
+
+private fun readStringMatrix(n: Int): MutableList<MutableList<String>> {
+    val matrix = mutableListOf<MutableList<String>>()
+    for (i in 0 until n) {
+        val list = readStringList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readIntMatrix(n: Int): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    for (i in 0 until n) {
+        val list = readIntList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readDoubleMatrix(n: Int): MutableList<MutableList<Double>> {
+    val matrix = mutableListOf<MutableList<Double>>()
+    for (i in 0 until n) {
+        val list = readDoubleList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readLongMatrix(n: Int): MutableList<MutableList<Long>> {
+    val matrix = mutableListOf<MutableList<Long>>()
+    for (i in 0 until n) {
+        val list = readLongList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
