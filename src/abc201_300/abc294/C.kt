86e97216ab1a0a88b06a963d1cb987eb5b904e07
k-negishi <56011598+k-negishi@users.noreply.github.com>
2023-04-23 00:40:44 +0900

    2023-04-23

---
+++ src/abc201_300/abc294/C.kt
@@ -0,0 +1,65 @@
+package abc201_300.abc294
+
+fun main() {
+    val (n, m) = readIntList()
+    val a = readLongList()
+    val b = readLongList()
+
+    val c = (a + b).sorted()
+    println(a.joinToString(" ") { "${c.binarySearch(it) + 1}" })
+    println(b.joinToString(" ") { "${c.binarySearch(it) + 1}" })
+}
+
+
+private fun read() = readLine()!!
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readStringList(): MutableList<String> = read().split(" ").toMutableList()
+
+private fun readIntList(): MutableList<Int> = readStringList().map { it.toInt() }.toMutableList()
+
+private fun readLongList(): MutableList<Long> = readStringList().map { it.toLong() }.toMutableList()
+
+private fun readDoubleList(): MutableList<Double> = readStringList().map { it.toDouble() }.toMutableList()
+
+private fun readStringMatrix(n: Int): MutableList<MutableList<String>> {
+    val matrix = mutableListOf<MutableList<String>>()
+    for (i in 0 until n) {
+        val list = readStringList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readIntMatrix(n: Int): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    for (i in 0 until n) {
+        val list = readIntList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readDoubleMatrix(n: Int): MutableList<MutableList<Double>> {
+    val matrix = mutableListOf<MutableList<Double>>()
+    for (i in 0 until n) {
+        val list = readDoubleList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readLongMatrix(n: Int): MutableList<MutableList<Long>> {
+    val matrix = mutableListOf<MutableList<Long>>()
+    for (i in 0 until n) {
+        val list = readLongList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
