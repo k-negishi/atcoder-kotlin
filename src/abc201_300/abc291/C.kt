12e6fa182ee45604435f4142191cc533b4f8d353
k-negishi <56011598+k-negishi@users.noreply.github.com>
2023-04-23 14:05:39 +0900

    2023-04-23

---
+++ src/abc201_300/abc291/C.kt
@@ -0,0 +1,78 @@
+package abc201_300.abc291
+
+fun main() {
+    val n = readInt()
+    val s = read()
+
+    var x = 0
+    var y = 0
+    val visited = mutableSetOf(listOf(0, 0))
+    for (it in s) { // forEachをforループに置き換え
+        when (it) {
+            'R' -> x++
+            'L' -> x--
+            'U' -> y++
+            'D' -> y--
+        }
+        if (visited.contains(listOf(x, y))) {
+            println("Yes")
+            return
+        }
+        visited.add(listOf(x, y))
+    }
+    println("No")
+
+}
+
+private fun read() = readLine()!!
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readStringList(): MutableList<String> = read().split(" ").toMutableList()
+
+private fun readIntList(): MutableList<Int> = readStringList().map { it.toInt() }.toMutableList()
+
+private fun readLongList(): MutableList<Long> = readStringList().map { it.toLong() }.toMutableList()
+
+private fun readDoubleList(): MutableList<Double> = readStringList().map { it.toDouble() }.toMutableList()
+
+private fun readStringMatrix(n: Int): MutableList<MutableList<String>> {
+    val matrix = mutableListOf<MutableList<String>>()
+    for (i in 0 until n) {
+        val list = readStringList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readIntMatrix(n: Int): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    for (i in 0 until n) {
+        val list = readIntList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readDoubleMatrix(n: Int): MutableList<MutableList<Double>> {
+    val matrix = mutableListOf<MutableList<Double>>()
+    for (i in 0 until n) {
+        val list = readDoubleList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readLongMatrix(n: Int): MutableList<MutableList<Long>> {
+    val matrix = mutableListOf<MutableList<Long>>()
+    for (i in 0 until n) {
+        val list = readLongList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
