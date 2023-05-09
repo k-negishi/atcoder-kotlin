2ab6312795ff85413100179ca44b065eb4d6c438
k-negishi <56011598+k-negishi@users.noreply.github.com>
2023-05-09 23:15:20 +0900

    2023-05-09

---
+++ src/abc201_300/abc287/B.kt
@@ -0,0 +1,75 @@
+package abc201_300.abc287
+
+@ExperimentalStdlibApi
+fun main() {
+    val (n, m) = readIntList()
+    val s = mutableListOf<String>()
+    val t = mutableListOf<String>()
+
+
+    repeat(n) {
+        s.add(read())
+    }
+
+    repeat(m) {
+        t.add(read())
+    }
+
+    val count = s.count { it.substring(3) in t }
+    println(count)
+
+}
+
+
+private fun read() = readLine()!!
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readStringList(): MutableList<String> = read().split(" ").toMutableList()
+
+private fun readIntList(): MutableList<Int> = readStringList().map { it.toInt() }.toMutableList()
+
+private fun readLongList(): MutableList<Long> = readStringList().map { it.toLong() }.toMutableList()
+
+private fun readDoubleList(): MutableList<Double> = readStringList().map { it.toDouble() }.toMutableList()
+
+private fun readStringMatrix(n: Int): MutableList<MutableList<String>> {
+    val matrix = mutableListOf<MutableList<String>>()
+    for (i in 0 until n) {
+        val list = readStringList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readIntMatrix(n: Int): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    for (i in 0 until n) {
+        val list = readIntList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readDoubleMatrix(n: Int): MutableList<MutableList<Double>> {
+    val matrix = mutableListOf<MutableList<Double>>()
+    for (i in 0 until n) {
+        val list = readDoubleList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readLongMatrix(n: Int): MutableList<MutableList<Long>> {
+    val matrix = mutableListOf<MutableList<Long>>()
+    for (i in 0 until n) {
+        val list = readLongList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
