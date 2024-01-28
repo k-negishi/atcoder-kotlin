49704d8fc3006c301f721804763210d02ce8597b
k-negishi <56011598+k-negishi@users.noreply.github.com>
2024-01-28 23:13:21 +0900

    ABC336

---
+++ src/abc301_400/abc336/D.kt
@@ -0,0 +1,157 @@
+package abc301_400.abc336
+
+fun main() {
+    var n = readInt()
+
+    var a = readIntList()
+    a = (listOf(0) + a + listOf(0)).toMutableList()
+    n += 2
+
+    val dr = MutableList(n) { 0 }
+    for (i in 1 until n) {
+        dr[i] = minOf(dr[i-1]+1, a[i])
+    }
+
+    val dl = MutableList(n) { 0 }
+    for (i in n-2 downTo 0) {
+        dl[i] = minOf(dl[i+1]+1, a[i])
+    }
+
+    var ans = 0
+    for (i in 0 until n) {
+        // iを中心としたときの最大値
+        val now = minOf(dl[i], dr[i])
+        ans = maxOf(ans, now)
+    }
+    println(ans)
+}
+
+
+private fun read() = readln()
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readStringList(): MutableList<String> = read().split(" ").toMutableList()
+
+private fun readIntList(): MutableList<Int> = readStringList().map { it.toInt() }.toMutableList()
+
+private fun readIntListFromOne(): MutableList<Int> {
+    val a = listOf(0)
+    val b = readStringList().map { it.toInt() }.toList()
+    return (a + b).toMutableList()
+}
+
+private fun readLongList(): MutableList<Long> = readStringList().map { it.toLong() }.toMutableList()
+
+private fun readLongListFromOne(): MutableList<Long> {
+    val a = listOf(0L)
+    val b = readStringList().map { it.toLong() }.toList()
+    return (a + b).toMutableList()
+}
+
+private fun readDoubleList(): MutableList<Double> = readStringList().map { it.toDouble() }.toMutableList()
+
+private fun readStringMatrix(n: Int): MutableList<MutableList<String>> {
+    val matrix = mutableListOf<MutableList<String>>()
+    for (i in 0 until n) {
+        val list = readStringList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readIntMatrix(n: Int): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    for (i in 0 until n) {
+        val list = readIntList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readDoubleMatrix(n: Int): MutableList<MutableList<Double>> {
+    val matrix = mutableListOf<MutableList<Double>>()
+    for (i in 0 until n) {
+        val list = readDoubleList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readLongMatrix(n: Int): MutableList<MutableList<Long>> {
+    val matrix = mutableListOf<MutableList<Long>>()
+    for (i in 0 until n) {
+        val list = readLongList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun List<Int>.lowerBound(value: Int): Int {
+    var left = 0
+    var right = this.size
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid] < value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
+private fun List<Long>.lowerBound(value: Long): Long {
+    var left = 0L
+    var right = this.size.toLong()
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid.toInt()] < value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
+private fun List<Int>.upperBound(value: Int): Int {
+    var left = 0
+    var right = this.size
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid] <= value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
+private fun List<Long>.upperBound(value: Long): Long {
+    var left = 0L
+    var right = this.size.toLong()
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid.toInt()] <= value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
