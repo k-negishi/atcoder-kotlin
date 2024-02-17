6a9bcb0ff6527a55d7d2bb856825bed1e48de3a0
k-negishi <56011598+k-negishi@users.noreply.github.com>
2024-02-17 23:16:13 +0900

    ABC341

---
+++ src/abc301_400/abc341/C.kt
@@ -0,0 +1,197 @@
+package abc301_400.abc341
+
+fun main() {
+    val (h, w, n) = readIntList()
+    val t = read()
+    val s = (0 until h).map {
+        read().map {
+            it.toString()
+        }
+    }
+
+    var ans = 0
+
+    for (row in 0 until h) {
+        for (col in 0 until w) {
+//            var genzaichi = s[row][col]
+            var nowRow = row
+            var nowCol = col
+            run {
+                t.forEachIndexed { index, arrow ->
+//                if (nowRow < 0 || h <= nowRow || nowCol < 0 || w <= nowCol ) {
+//                    return@forEachIndexed
+//                }
+                    if (s[nowRow][nowCol] == "#") {
+                        return@run
+                    }
+
+                    if (arrow == 'L') {
+                        if (s[nowRow][nowCol-1] == ".") {
+                            nowCol--
+                        } else {
+                            return@run
+                        }
+                    }
+                    if (arrow == 'U') {
+                        if (s[nowRow -1][nowCol] == ".") {
+                            nowRow--
+                        } else {
+                            return@run
+                        }
+                    }
+                    if (arrow == 'R') {
+                        if (s[nowRow][nowCol+1] == ".") {
+                            nowCol++
+                        } else {
+                            return@run
+                        }
+                    }
+                    if (arrow == 'D') {
+                        if (s[nowRow+1][nowCol] == ".") {
+                            nowRow++
+                        } else {
+                            return@run
+                        }
+                    }
+                    if (index == t.length-1) {
+                        ans++
+                    }
+                }
+            }
+        }
+    }
+
+    println(ans)
+
+
+}
+
+
+private fun read() = readln()
+
+private fun readInt() = read().toInt()
+
+private fun readLong() = read().toLong()
+
+private fun readDouble() = read().toDouble()
+
+private fun readStringList(): MutableList<String> = read().split(" ").toMutableList()
+
+private fun readIntList(): MutableList<Int> = readStringList().map { it.toInt() }.toMutableList()
+
+private fun readIntListFromOne(): MutableList<Int> {
+    val a = listOf(0)
+    val b = readStringList().map { it.toInt() }.toList()
+    return (a + b).toMutableList()
+}
+
+private fun readLongList(): MutableList<Long> = readStringList().map { it.toLong() }.toMutableList()
+
+private fun readLongListFromOne(): MutableList<Long> {
+    val a = listOf(0L)
+    val b = readStringList().map { it.toLong() }.toList()
+    return (a + b).toMutableList()
+}
+
+private fun readDoubleList(): MutableList<Double> = readStringList().map { it.toDouble() }.toMutableList()
+
+private fun readStringMatrix(n: Int): MutableList<MutableList<String>> {
+    val matrix = mutableListOf<MutableList<String>>()
+    for (i in 0 until n) {
+        val list = readStringList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readIntMatrix(n: Int): MutableList<MutableList<Int>> {
+    val matrix = mutableListOf<MutableList<Int>>()
+    for (i in 0 until n) {
+        val list = readIntList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readDoubleMatrix(n: Int): MutableList<MutableList<Double>> {
+    val matrix = mutableListOf<MutableList<Double>>()
+    for (i in 0 until n) {
+        val list = readDoubleList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun readLongMatrix(n: Int): MutableList<MutableList<Long>> {
+    val matrix = mutableListOf<MutableList<Long>>()
+    for (i in 0 until n) {
+        val list = readLongList()
+        matrix.add(list)
+    }
+    return matrix
+}
+
+private fun List<Int>.lowerBound(value: Int): Int {
+    var left = 0
+    var right = this.size
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid] < value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
+private fun List<Long>.lowerBound(value: Long): Long {
+    var left = 0L
+    var right = this.size.toLong()
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid.toInt()] < value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
+private fun List<Int>.upperBound(value: Int): Int {
+    var left = 0
+    var right = this.size
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid] <= value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
+private fun List<Long>.upperBound(value: Long): Long {
+    var left = 0L
+    var right = this.size.toLong()
+
+    while (left < right) {
+        val mid = left + (right - left) / 2
+        if (this[mid.toInt()] <= value) {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+
+    return left
+}
+
